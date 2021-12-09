dea9ef42e8254167fc3d55473d4f00d58a80f971
davidciliberto1 <davidciliberto@gmail.com>
2021-12-09 18:21:03 -0300

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onInputChange = (e) => {
+        this.setState({ term: e.target.value })
+    };
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        this.props.onFormSubmit(this.state.term);
+
+        // TO DO make sure to make callbak
+    };
+    render() {
+        return (
+            <div className='search-bar ui segment' >
+                <form 
+                className='ui form'
+                onSubmit={this.onFormSubmit}
+                >
+                    <div className='field'>
+                        <label>video search</label>
+                        <input
+                            type='text'
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+export default SearchBar;
